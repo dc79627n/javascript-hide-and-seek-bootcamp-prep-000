37e52a08a3d20be9fe83d1a6e25660dfe84c1f3b
Derrick Chu <dc79627n@pace.edu>
2017-02-10 00:33:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+//function getFirstSelector retrieves first elements
+
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+    for(var i = 0; i < list.length;i++){
+        list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString();
+    }
+ }
