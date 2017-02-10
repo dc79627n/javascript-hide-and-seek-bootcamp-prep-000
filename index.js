dedead26030058251b9358f72e1213c2d6ce1790
Derrick Chu <dc79627n@pace.edu>
2017-02-10 00:34:30 +0000

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function deepestChild() {
 }
 
 function increaseRankBy(n){
-  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
     for(var i = 0; i < list.length;i++){
         list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString();
     }
